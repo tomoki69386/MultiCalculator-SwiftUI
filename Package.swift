d8a01a383380dd93f0fa9c500acf2d1b79a1eb63
tomoki_sun <tomoki69386@gmail.com>
2021-07-28 05:47:38 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let package = Package(
+var package = Package(
   name: "Package",
   platforms: [
     .macOS(.v11),
@@ -11,19 +11,6 @@ let package = Package(
   ],
   products: [
     .library(name: "AppFeature", targets: ["AppFeature"]),
-    .library(name: "CalculatorFeature", targets: ["CalculatorFeature"]),
-    .library(name: "MultiCalculatorFeature", targets: ["MultiCalculatorFeature"]),
-    .library(name: "SettingFeature", targets: ["SettingFeature"]),
-
-    .library(name: "Calculator", targets: ["Calculator"]),
-    .library(name: "SnapshotTestHelper", targets: ["SnapshotTestHelper"]),
-    .library(name: "Build", targets: ["Build"]),
-    .library(name: "Styleguide", targets: ["Styleguide"]),
-    .library(name: "DeviceStateModifier", targets: ["DeviceStateModifier"]),
-    .library(name: "FeedbackGeneratorClient", targets: ["FeedbackGeneratorClient"]),
-    .library(name: "UserDefaultsClient", targets: ["UserDefaultsClient"]),
-    .library(name: "UIApplicationClient", targets: ["UIApplicationClient"]),
-    .library(name: "StoreKitClient", targets: ["StoreKitClient"]),
   ],
   dependencies: [
     .package(
@@ -45,85 +32,116 @@ let package = Package(
         .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
       ]
     ),
-    .target(
-      name: "CalculatorFeature",
-      dependencies: [
-        "Calculator",
-        "FeedbackGeneratorClient",
-        "UserDefaultsClient",
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-      ]
-    ),
-    .testTarget(
-      name: "CalculatorFeatureTests",
-      dependencies: [
-        "CalculatorFeature",
-        "SnapshotTestHelper",
-        "DeviceStateModifier",
-        .product(name: "SnapshotTesting", package: "SnapshotTesting"),
-      ],
-      exclude: [
-        "__Snapshots__"
-      ]
-    ),
-    .target(
-      name: "MultiCalculatorFeature",
-      dependencies: [
-        "CalculatorFeature",
-        "DeviceStateModifier",
-        "FeedbackGeneratorClient",
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-      ]
-    ),
-    .target(name: "Calculator"),
-    .testTarget(
-      name: "CalculatorTests",
-      dependencies: [
-        "Calculator"
-      ]
-    ),
-    .target(
-      name: "SettingFeature",
-      dependencies: [
-        "Build",
-        "Styleguide",
-        "UIApplicationClient",
-        "UserDefaultsClient",
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
-      ]
-    ),
-    .target(
-      name: "SnapshotTestHelper",
-      dependencies: [
-        .product(name: "SnapshotTesting", package: "SnapshotTesting")
-      ]
-    ),
-    .target(name: "Build"),
-    .target(name: "Styleguide"),
-    .target(name: "DeviceStateModifier"),
-    .target(
-      name: "FeedbackGeneratorClient",
-      dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-      ]
-    ),
-    .target(
-      name: "UserDefaultsClient",
-      dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-      ]
-    ),
-    .target(
-      name: "UIApplicationClient",
-      dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-      ]
-    ),
-    .target(
-      name: "StoreKitClient",
-      dependencies: [
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-      ]
-    ),
   ]
 )
+
+// MARK: - feature
+package.products.append(contentsOf: [
+  .library(name: "CalculatorFeature", targets: ["CalculatorFeature"]),
+  .library(name: "MultiCalculatorFeature", targets: ["MultiCalculatorFeature"]),
+  .library(name: "SettingFeature", targets: ["SettingFeature"]),
+])
+
+package.targets.append(contentsOf: [
+  .target(
+    name: "CalculatorFeature",
+    dependencies: [
+      "Calculator",
+      "FeedbackGeneratorClient",
+      "UserDefaultsClient",
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]
+  ),
+  .testTarget(
+    name: "CalculatorFeatureTests",
+    dependencies: [
+      "CalculatorFeature",
+      "SnapshotTestHelper",
+      "DeviceStateModifier",
+      .product(name: "SnapshotTesting", package: "SnapshotTesting"),
+    ],
+    exclude: [
+      "__Snapshots__"
+    ]
+  ),
+  .target(
+    name: "MultiCalculatorFeature",
+    dependencies: [
+      "CalculatorFeature",
+      "DeviceStateModifier",
+      "FeedbackGeneratorClient",
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]
+  ),
+  .testTarget(
+    name: "CalculatorTests",
+    dependencies: [
+      "Calculator"
+    ]
+  ),
+  .target(
+    name: "SettingFeature",
+    dependencies: [
+      "Build",
+      "Styleguide",
+      "UIApplicationClient",
+      "UserDefaultsClient",
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+    ]
+  ),
+])
+
+// MARK: - client
+package.products.append(contentsOf: [
+  .library(name: "Calculator", targets: ["Calculator"]),
+  .library(name: "SnapshotTestHelper", targets: ["SnapshotTestHelper"]),
+  .library(name: "Build", targets: ["Build"]),
+  .library(name: "Styleguide", targets: ["Styleguide"]),
+  .library(name: "DeviceStateModifier", targets: ["DeviceStateModifier"]),
+  .library(name: "FeedbackGeneratorClient", targets: ["FeedbackGeneratorClient"]),
+  .library(name: "UserDefaultsClient", targets: ["UserDefaultsClient"]),
+  .library(name: "UIApplicationClient", targets: ["UIApplicationClient"]),
+  .library(name: "StoreKitClient", targets: ["StoreKitClient"]),
+])
+
+package.targets.append(contentsOf: [
+  .target(name: "Calculator"),
+  .target(
+    name: "SnapshotTestHelper",
+    dependencies: [
+      .product(name: "SnapshotTesting", package: "SnapshotTesting")
+    ]
+  ),
+  .target(name: "Build"),
+  .target(name: "Styleguide"),
+  .target(name: "DeviceStateModifier"),
+  .target(
+    name: "FeedbackGeneratorClient",
+    dependencies: [
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+    ]
+  ),
+  .target(
+    name: "UserDefaultsClient",
+    dependencies: [
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+    ]
+  ),
+  .target(
+    name: "UIApplicationClient",
+    dependencies: [
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+    ]
+  ),
+  .target(
+    name: "StoreKitClient",
+    dependencies: [
+      .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+    ]
+  ),
+])
+
+// MARK: - test tools
+package.targets.append(contentsOf: [
+  .testTarget(name: "AppStoreSnapshot")
+])
